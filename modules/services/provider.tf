f1174472aab20e29f909c938d36627ac9ba76884
sibalgm <gener.sibal@reedelsevier.com>
2022-08-27 16:59:31 +0800

    main and provider file added

---
+++ modules/services/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+    required_providers {
+        aws = {
+            source= "hashicorp/aws"
+            version= "~> 4.0"
+        }
+    }
+
+}
+
+provider "aws" {
+    region = {}
+    access_key = "AKIAT2JERSEINDGYDAG7"
+    secret_key = "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"
+  
+}
+
